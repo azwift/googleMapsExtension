5d95c1ae08c4c6ec85ae0054682d25e8f9291fd1
azwift <ali_bitar93@hotmail.com>
2018-09-23 12:59:20 -0400

    local server support

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.listen('3000');
 console.log('working on 3000');
